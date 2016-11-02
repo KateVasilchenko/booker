781a75887e7db8d3ea5e2a24cf0fc3436bb96df6
reillyethan <lekskazimirchuk@gmail.com>
2016-11-02 16:50:24 +0200

    Fixed charts

--- app/services/charts.js
+++ app/services/charts.js
@@ -2,54 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   radarDefaultDataset: {
-    backgroundColor: [],
-    hoverBackgroundColor: [],
-    data: []
-  },
-  lineDefaultDataset: {
-    fill: true,
-    lineTension: 0.1,
-    backgroundColor: 'rgba(75,192,192,0.4)',
-    borderColor: 'rgba(75,192,192,1)',
-    borderCapStyle: 'butt',
-    borderDash: [],
-    borderDashOffset: 0.0,
-    borderJoinStyle: 'miter',
-    pointBorderColor: 'rgba(75,192,192,1)',
-    pointBackgroundColor: '#fff',
-    pointBorderWidth: 1,
-    pointHoverRadius: 5,
-    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-    pointHoverBorderColor: 'rgba(220,220,220,1)',
-    pointHoverBorderWidth: 2,
-    pointRadius: 1,
-    pointHitRadius: 10,
-    data: [],
-    spanGaps: false
+    label: null,
+    backgroundColor: null,
+    borderColor: null,
+    pointBackgroundColor: null,
+    pointBorderColor: null,
+    pointHoverBackgroundColor: "#fff",
+    pointHoverBorderColor: "rgba(179,181,198,1)",
+    pointBorderWidth: 4,
+    borderWidth: 1,
+    data: [] //pass
   },
   prepareData(entities) {
     let incomeDataset = Ember.copy(this.get('radarDefaultDataset'), true);
     incomeDataset.label = 'Income';
     let outcomeDataset = Ember.copy(this.get('radarDefaultDataset'), true);
     outcomeDataset.label = 'Outcome';
-    let lineDataset = this.get('lineDefaultDataset');
-    lineDataset.label = 'Balance';
     let categoriesNames = [];
-    let monthsNames = [];
-    entities.transactions.sortBy('createdAt').forEach(function (transaction) {
-      const monthName = moment(transaction.get('createdAt')).format('MMMM');
-      if (monthsNames.indexOf(monthName) === -1) {
-        monthsNames.push(monthName);
-      }
-      let balance = 0;
-      if (!transaction.get('isNegative')) {
-        balance += parseFloat(transaction.get('amount'));
-      } else {
-        balance -= parseFloat(transaction.get('amount'));
-      }
-      lineDataset.data.push(balance);
-    });
-
     entities.categories.forEach(function (category) {
       if (category.get('transactions.length')) {
         categoriesNames.push(category.get('name'));
@@ -63,11 +32,15 @@ export default Ember.Service.extend({
           }
         });
         incomeDataset.data.push(incomeBalance);
-        incomeDataset.backgroundColor.push(category.get('color'));
-        incomeDataset.hoverBackgroundColor.push(category.get('color'));
+        incomeDataset.backgroundColor = 'rgba(59, 103, 208, 0.61)';
+        incomeDataset.pointBackgroundColor = '#4e6cb3';
+        incomeDataset.pointBorderColor = 'rgba(59, 103, 208, 0.61)';
+        incomeDataset.borderColor = '#4e6cb3';
         outcomeDataset.data.push(outcomeBalance);
-        outcomeDataset.backgroundColor.push(category.get('color'));
-        outcomeDataset.hoverBackgroundColor.push(category.get('color'));
+        outcomeDataset.backgroundColor = 'rgba(200, 116, 200, 0.48)';
+        outcomeDataset.pointBackgroundColor = '#ca70c5';
+        outcomeDataset.pointBorderColor = 'rgba(200, 116, 200, 0.48)';
+        outcomeDataset.borderColor = '#ca70c5';
       }
     });
 
@@ -80,10 +53,6 @@ export default Ember.Service.extend({
       outcomeData: {
         labels: categoriesNames,
         datasets: [outcomeDataset]
-      },
-      balanceData: {
-        labels: monthsNames,
-        datasets: [lineDataset]
       }
     }
   }
