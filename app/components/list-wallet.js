dadeb4bfc13e5338bd9078e0fbabf903a483c6aa
reillyethan <lekskazimirchuk@gmail.com>
2016-12-08 11:02:00 +0200

    Added wallets route.

---
+++ app/components/list-wallet.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  walletsRaw: null, //pass
+
+  wallets: Ember.computed('walletsRaw.[]', function () {
+    return this.get('walletsRaw').rejectBy('isDeleted');
+  }),
+
+  modelToDelete: null,
+  isHidden: true,
+
+  actions: {
+    cancelDelete() {
+      this.set('isHidden', true);
+    },
+    deleteRecord(model) {
+      this.set('isHidden', true);
+      this.get('wallets').removeObject(model);
+      model.destroyRecord();
+      this.set('modelToDelete', null);
+    },
+    edit(wallet) {
+      let controller = this.get('own').lookup('controller:application');
+
+      if (!controller.get('disabledButtons')) {
+        controller.set('disabledButtons', true);
+        controller.setProperties('wallet', wallet);
+        controller.set('hidden', false);
+      }
+    },
+    delete(wallet) {
+      this.set('modelToDelete', wallet);
+      this.set('isHidden', false);
+    }
+  }
+});
--- app/routes/wallets.js
+++ app/routes/wallets.js
@@ -2,12 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll("transaction");
+    return this.store.findAll("wallet");
   },
   setupController(controller, model) {
     this._super(controller, model);
     controller.set('own', Ember.getOwner(this));
-    controller.set('transactions', model);
-    controller.set('categories', this.store.peekAll('category'));
+    controller.set('wallets', model);
   }
 });
