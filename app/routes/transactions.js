46ab76ee33bc9f1fce8ffb4c83e1e8284538d12d
reillyethan <lekskazimirchuk@gmail.com>
2016-12-12 15:45:39 +0200

    Completely removed usage of controllers as the storage.

--- app/routes/transactions.js
+++ app/routes/transactions.js
@@ -2,12 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll("transaction");
-  },
-  setupController(controller, model) {
-    this._super(controller, model);
-    controller.set('own', Ember.getOwner(this));
-    controller.set('transactions', model);
-    controller.set('categories', this.store.peekAll('category'));
+    return Ember.RSVP.hash({
+      transactions: this.store.findAll("transaction"),
+      own: Ember.getOwner(this),
+      categories: this.store.peekAll('category')
+    });
   }
 });
--- app/routes/wallets.js
+++ app/routes/wallets.js
@@ -2,11 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll("wallet");
-  },
-  setupController(controller, model) {
-    this._super(controller, model);
-    controller.set('own', Ember.getOwner(this));
-    controller.set('wallets', model);
+    return Ember.RSVP.hash({
+      wallets: this.store.findAll("wallet"),
+      own: Ember.getOwner(this)
+    });
   }
 });
