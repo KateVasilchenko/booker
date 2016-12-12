09a62d96015735627383c32034563cf6ebce3e3c
reillyethan <lekskazimirchuk@gmail.com>
2016-12-12 15:55:11 +0200

    FIxed serializers.

--- app/serializers/category.js
+++ app/serializers/category.js
@@ -2,9 +2,20 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    for (let i=0; i<payload.categories.length; i++) {
-      payload.categories[i].transactions = Ember.copy(payload.categories[i].transactionIds);
-      delete payload.categories[i].transactionIds;
+    if (payload.categories !== undefined) {
+      for (let i=0; i<payload.categories.length; i++) {
+        if (payload.categories[i].transactions === undefined &&
+          payload.categories[i].transactionIds !== undefined) {
+          payload.categories[i].transactions = Ember.copy(payload.categories[i].transactionIds);
+          delete payload.categories[i].transactionIds;
+        }
+      }
+    } else if (payload.category !== undefined) {
+      if (payload.category.transactions === undefined &&
+        payload.category.transactionIds !== undefined) {
+        payload.category.transactions = Ember.copy(payload.category.transactionIds);
+        delete payload.category.transactionIds;
+      }
     }
     return this._super(...arguments);
   },
--- app/serializers/wallet.js
+++ app/serializers/wallet.js
@@ -2,9 +2,20 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    for (let i=0; i<payload.wallets.length; i++) {
-      payload.wallets[i].transactions = Ember.copy(payload.wallets[i].transactionIds);
-      delete payload.wallets[i].transactionIds;
+    if (payload.wallets !== undefined) {
+      for (let i=0; i<payload.wallets.length; i++) {
+        if (payload.wallets[i].transactions === undefined &&
+          payload.wallets[i].transactionIds !== undefined) {
+          payload.wallets[i].transactions = Ember.copy(payload.wallets[i].transactionIds);
+          delete payload.wallets[i].transactionIds;
+        }
+      }
+    } else if (payload.wallet !== undefined) {
+      if (payload.wallet.transactions === undefined &&
+        payload.wallet.transactionIds !== undefined) {
+        payload.wallet.transactions = Ember.copy(payload.wallet.transactionIds);
+        delete payload.wallet.transactionIds;
+      }
     }
     return this._super(...arguments);
   },
