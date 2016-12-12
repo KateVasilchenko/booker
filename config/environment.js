a353396e1d79eaf639089934ff2e4cc8ccea3e5c
reillyethan <lekskazimirchuk@gmail.com>
2016-12-12 10:39:21 +0200

    Removed deprecation and csp messages.

--- config/environment.js
+++ config/environment.js
@@ -18,19 +18,21 @@ module.exports = function(environment) {
     }
   };
 
+  ENV.contentSecurityPolicy = {
+    'default-src': "'none'",
+    'img-src': "'self' data:",
+    'script-src': "'self' 'unsafe-eval' 'unsafe-inline' 192.168.33.10:" + process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT,
+    'connect-src': "'self' ws://192.168.33.10:" + process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT,
+    'font-src': "'self' https://fonts.gstatic.com",
+    'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com"
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.contentSecurityPolicy = {
-      'default-src': "'none'",
-      'script-src': "'self' 'unsafe-eval' 'unsafe-inline' 192.168.33.10:" + process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT,
-      'connect-src': "'self' ws://192.168.33.10:" + process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT,
-      'font-src': "'self'",
-      'style-src': "'self' 'unsafe-inline'"
-    }
   }
 
   if (environment === 'test') {
