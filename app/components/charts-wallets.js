ba15349aa3ef05a567c993796b8c275275543139
Leks Kazimirchuk <lekskazimirchuk@gmail.com>
2016-12-26 16:04:01 +0200

    Added mobile charts.

--- app/components/charts-wallets.js
+++ app/components/charts-wallets.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+// import Highcharts from 'ember-highcharts'; TODO: check if it works without it.
 
 export default Ember.Component.extend({
   chartCategories: null, // pass
@@ -6,6 +7,92 @@ export default Ember.Component.extend({
   chartOutcomeData: null, // pass
 
   tagName: '',
+
+  mobileChartOptions: {
+    chart: {
+      height: 200,
+      type: 'line'
+    },
+    title: {
+      align: 'left',
+      x: 24,
+      style: {
+        color: '' // pass
+      },
+      text: '', // pass
+    },
+    tooltip: {
+      backgroundColor: '', // pass
+      style: {
+        color: 'white'
+      },
+      headerFormat: '',
+      useHTML: true,
+      shared: false,
+      formatter: function() {
+        return '<span style="font-size: 10px; text-align: center">' +
+          this.point.name +
+          '</span></br><b style="font-size: 16px; text-align: center;">' +
+          this.y.toString() +
+          '</b>';
+      }
+    },
+    xAxis: {
+      categories: [], // pass
+      tickLength: 0,
+      // labels: {
+      //   enabled: false
+      // }
+    },
+    yAxis: [{
+      endOnTick: false,
+      gridLineWidth: 0,
+      lineWidth: 1,
+      title: {
+        text: ''
+      },
+      labels: {
+        enabled: false
+      }
+    }],
+  },
+  mobileChartData: [{
+    data: []
+  }],
+  mobileChartTheme: {
+    legend: false,
+    plotOptions: {
+      area: {
+        fillColor: {
+          linearGradient: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1
+          },
+          stops: [
+            [0, Highcharts.getOptions().colors[0]],
+            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+          ]
+        },
+        marker: {
+          radius: 2
+        },
+        lineWidth: 1,
+        states: {
+          hover: {
+            lineWidth: 1
+          }
+        },
+        threshold: null
+      },
+      series: {
+        borderRadius: 5,
+        pointWidth: 8,
+        color: '', // pass
+      }
+    }
+  },
 
   chartOptions: {
     chart: {
@@ -97,6 +184,13 @@ export default Ember.Component.extend({
     outcome: null
   },
 
+  mobileCharts: {
+    income: null,
+    outcome: null
+  },
+
+  mobileView: false,
+
   init() {
     this._super(...arguments);
     this.set('charts', {
@@ -109,6 +203,19 @@ export default Ember.Component.extend({
         chartOptions: Ember.copy(this.get('chartOptions'), true),
         chartData: Ember.copy(this.get('chartData'), true),
         chartTheme: Ember.copy(this.get('chartTheme'), true)
+      }
+    });
+
+    this.set('mobileCharts', {
+      income: {
+        chartOptions: Ember.copy(this.get('mobileChartOptions'), true),
+        chartData: Ember.copy(this.get('mobileChartData'), true),
+        chartTheme: Ember.copy(this.get('mobileChartTheme'), true)
+      },
+      outcome: {
+        chartOptions: Ember.copy(this.get('mobileChartOptions'), true),
+        chartData: Ember.copy(this.get('mobileChartData'), true),
+        chartTheme: Ember.copy(this.get('mobileChartTheme'), true)
       }
     });
 
@@ -137,5 +244,35 @@ export default Ember.Component.extend({
     outcomeChart.chartOptions.xAxis.categories = this.get('chartCategories');
     outcomeChart.chartData[1].data = this.get('chartOutcomeData');
     outcomeChart.chartData[0].data = this.get('chartMonthOutcomeData');
+
+    let mobileIncomeChart = this.get('mobileCharts.income');
+    mobileIncomeChart.chartOptions.title.text = 'Income ' + totalIncome.toFixed(2).toString() + ' USD';
+    mobileIncomeChart.chartOptions.title.style.color = '#63A9A9';
+    mobileIncomeChart.chartOptions.tooltip.backgroundColor = '#63A9A9';
+    mobileIncomeChart.chartTheme.plotOptions.series.color = '#63A9A9';
+    mobileIncomeChart.chartOptions.xAxis.categories = this.get('chartCategories');
+    mobileIncomeChart.chartData[0].data = this.get('chartIncomeData');
+
+    let mobileOutcomeChart = this.get('mobileCharts.outcome');
+    mobileOutcomeChart.chartOptions.title.text = 'Expenses ' + totalOutcome.toFixed(2).toString() + ' USD';
+    mobileOutcomeChart.chartOptions.title.style.color = '#ae84c6';
+    mobileOutcomeChart.chartOptions.tooltip.backgroundColor = '#ae84c6';
+    mobileOutcomeChart.chartTheme.plotOptions.series.color = '#ae84c6';
+    mobileOutcomeChart.chartOptions.xAxis.categories = this.get('chartCategories');
+    mobileOutcomeChart.chartData[0].data = this.get('chartOutcomeData');
+
+    if (window.innerWidth < 767) {
+      this.set('mobileView', true);
+    } else if (window.innerWidth >= 767) {
+      this.set('mobileView', false);
+    }
+
+    Ember.$(window).resize(() => {
+      if (window.innerWidth < 767) {
+        this.set('mobileView', true);
+      } else if (window.innerWidth >= 767) {
+        this.set('mobileView', false);
+      }
+    });
   }
 });
